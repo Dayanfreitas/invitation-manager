db2ceed000982d107355e6b090b3f90395a5247a
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-08-27 09:05:55 -0300

    chore: setup test with rspec, simplecov, fuubar

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+ENV["RAILS_ENV"] = 'test'
+require 'shoulda/matchers'
+
+require 'support/rspec-rails'
+require 'support/simplecov'
+require 'support/fuubar'
+
+require "rspec/json_expectations"
+
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = [:expect, :should]
+  end
+
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+
+  config.mock_with :rspec
+
+  config.example_status_persistence_file_path = 'spec/specs_reports.txt'
+end
