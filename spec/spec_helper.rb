105f50205e814bd4b5246e87773009d26b0f774e
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-08-27 21:19:17 -0300

    chore: Shoulda::Matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,12 @@ require 'support/fuubar'
 
 require "rspec/json_expectations"
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
