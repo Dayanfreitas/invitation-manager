d3cb9b50c6b35eb38ba54a69c1bd14ad2a9f33a0
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-08-31 00:28:07 -0300

    test:

--- spec/controller/companies_controller_spec.rb
+++ spec/controller/companies_controller_spec.rb
@@ -20,5 +20,33 @@ RSpec.describe CompaniesController, type: :controller do
 
       expect(assigns(:companies)).to eq([company])
     end
+
+    it "should not return all companies" do
+      get :index
+
+      expect(assigns(:companies)).to be_empty
+    end
+  end
+
+  context "#GET #show" do
+    it "should success render to show" do
+      company = FactoryBot.create(:company)
+      get :show, params: { id: company.id }
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return company" do
+      company = FactoryBot.create(:company)
+      get :show, params: { id: company.id }
+
+      expect(assigns(:company)).to eq(company)
+    end
+
+    it "should not return company" do
+      get :show, params: { id: 0 }
+
+      expect(assigns(:company)).to be_nil
+    end
   end
 end
