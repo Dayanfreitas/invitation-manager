4c7605528b15984895c41525757a7136ec7c90a9
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-08-31 01:57:10 -0300

    fix: company spec with device

--- spec/controller/companies_controller_spec.rb
+++ spec/controller/companies_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
+
+  before(:each) do
+    sign_in FactoryBot.create(:user)
+  end
+
   context "#GET index" do
     it "should success render to index" do
       get :index
