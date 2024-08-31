4f35c110f8382b1fd6610a1619ca9f1198269541
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-08-30 23:30:23 -0300

    feat: 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found
+    return render 'errors/404', layout: false, status: 404
+  end
 end
