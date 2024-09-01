b08f39ed72857080ce14b3340bb3fc29a7bf568f
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-09-01 01:41:04 -0300

    feat: usage login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,18 +4,23 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    # TODO: refactor this check to a service
+    @token = params[:invitation_token]
+    if @token.present? && Invitation.find_by(token: @token)&.user&.email.present?
+      sign_in(Invitation.find_by(token: @token)&.user)
+      return redirect_to root_path
+    end
+   
+    super
+  end
 
   # POST /resource/sign_in
   # def create
   #   super
   # end
 
-  # DELETE /resource/sign_out
   def destroy
-    byebug
     super
   end
 
