240b78aa82d464aac413793b45e14ae573fff5ec
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-09-02 19:49:52 -0300

    feat: i18n

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Www
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
+    # config.i18n.locale = "pt-BR" 
+    config.i18n.default_locale = "pt-BR"
+
     config.autoload_paths += [
       config.root.join('lib', 'application', 'query_builder').to_s,
       config.root.join('lib', 'application', 'repository').to_s,
