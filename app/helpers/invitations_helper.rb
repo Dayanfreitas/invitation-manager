8d0d43ddaa9c3b9f69e7ee3e74ed61fe7aff3fe5
Dayanfreitas <dayan.freitas.df@gmail.com>
2024-09-02 21:13:31 -0300

    fix: layout and i18n

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,2 +1,8 @@
 module InvitationsHelper
+  def options_for_status
+    Invitation::Status.constants.map do |status|
+      value = status.downcase
+      [t("views.invitation.status.#{value}"), value]
+    end
+  end
 end
